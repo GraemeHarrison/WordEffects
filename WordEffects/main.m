faefe48a983c7a961cbb7147f4aef12b022e0c13
Graeme Harrison <gharrison365@gmail.com>
2016-01-11 17:52:17 -0500

    Added Respond feature

--- WordEffects/main.m
+++ WordEffects/main.m
@@ -29,7 +29,7 @@ int main () {
     printf("Your string is %s\n", inputChars);
     
     // convert char array to an NSString object
-    NSString *inputString = [NSString stringWithUTF8String:inputChars];
+    NSString *inputString = [[NSString stringWithUTF8String:inputChars] stringByReplacingOccurrencesOfString:@"\n" withString:@""];
     
     // If string is a number do this:
     if ([inputString rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"0123456789"]].location != NSNotFound) {
@@ -45,19 +45,23 @@ int main () {
         NSLog(@"Input was %@", inputString);
         
         // convert inputString to uppercase and print it
-        inputString = [inputString uppercaseString];
-        NSLog(@"Make it louder: %@", inputString);
+        //inputString = [inputString uppercaseString];
+        NSLog(@"Make it louder: %@", [inputString uppercaseString]);
         
         // convert inputString to lowercase and print it
-        inputString = [inputString lowercaseString];
-        NSLog(@"Make it quieter: %@", inputString);
+        //inputString = [inputString lowercaseString];
+        NSLog(@"Make it quieter: %@", [inputString lowercaseString]);
         
         // Canadianize string
         NSString *canada = @"eh?";
-        inputString = [inputString stringByAppendingString:canada];
-        NSLog(@"Make it Canadian: %@", inputString);
+        //inputString = [inputString stringByAppendingString:canada];
+        NSLog(@"Make it Canadian: %@", [inputString stringByAppendingString:canada]);
         
+        // Respond to quesitons and exclamations
+        if ([inputString hasSuffix:@"?"]) {
+            NSLog(@"I don't know");
+        } else if ([inputString hasSuffix:@"!"])
+            NSLog(@"Whoa, calm down!");
     }
-    
     return 0;
 }
