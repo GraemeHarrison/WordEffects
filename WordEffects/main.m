1574aef28624a03364d91d7b0cac540c9bc72b41
Graeme Harrison <gharrison365@gmail.com>
2016-01-11 16:49:44 -0500

    first commit

---
+++ WordEffects/main.m
@@ -0,0 +1,38 @@
+//
+//  main.m
+//  WordEffects
+//
+//  Created by Graeme Harrison on 2016-01-11.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//int main(int argc, const char * argv[]) {
+//    @autoreleasepool {
+//        // insert code here...
+//        NSLog(@"Hello, World!");
+//    }
+//    return 0;
+//}
+
+int main () {
+    
+    //255 unit long array of characters
+    char inputChars[255];
+    
+    printf("Input a string: ");
+    // limts input to max 255 characters
+    fgets(inputChars, 255, stdin);
+    
+    // print as a c string
+    printf("Your string is %s\n", inputChars);
+    
+    // convert char array to an NSString object
+    NSString *inputString = [NSString stringWithUTF8String:inputChars];
+    
+    // print NSString object
+    NSLog(@"Input was %@", inputString);
+    
+    return 0;
+}
